f554c4f1a42a8c426e16441ba2a6c84312bcea33
succynice <succynice@gmail.com>
2022-11-06 22:36:08 +0100

    strace

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+# strace
+# Fix 500 error when a GET HTTP method is requested to Apache web server
+
+exec { 'fixed-phpp':
+  command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
+  path    => '/bin';
+}
